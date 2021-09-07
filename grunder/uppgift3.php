7ae27258f79ffb957042240f4cd1835323ee2cd0
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-09-07 13:48:58 +0200

    php grunder

---
+++ grunder/uppgift3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    $githubNamn = "LC022867";
+    $githubUrl = "https://github.com/LC022867/EE19-ar3";
+    echo "Min repos på github heter $githubNamn och adress dit är $githubUrl.";
+    ?>
+</body>
+</html>
