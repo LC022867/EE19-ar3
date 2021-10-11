60cc27a5a74ddfd0c74af8e73572a4abfbb3f2b2
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-10-11 14:42:40 +0200

    formulärs uppgift 12

---
+++ formuler-uppgifter/uppgift12/backend.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Temperaturomvandlare</title>
+    <link rel="stylesheet" href="../bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <h3>Resultat</h3>
+
+        <?php
+        $text = filter_input(INPUT_POST, 'text', FILTER_SANITIZE_STRING);
+        $riktning = filter_input(INPUT_POST, 'riktning', FILTER_SANITIZE_STRING);
+
+        // OM riktning är l->u (lowercase to uppercase)
+        if ($riktning == "lu") {
+            $str = strtoupper($text);
+            echo $str;
+        } else {
+            $str = strtolower($text);
+            echo $str;
+        }
+        ?>
+    </div>
+</body>
+</html>
---
+++ formuler-uppgifter/uppgift12/frontend.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Temperaturomvandlare</title>
+    <link rel="stylesheet" href="../bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="kontainer">
+        <h3>Omvandla text</h3>
+        <form action="backend.php" method="POST">
+            <div class="mb-3">
+                <label for="textarea" class="form-label">Mata in en text</label>
+                <textarea type="text" class="form-control" id="textarea" name="text" rows="10"></textarea>
+            </div>
+            <div class="kol2">
+                <p>Versaler eller Gemener?</p>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="lu" checked name="riktning" value="lu">
+                    <label class="form-check-label" for="lu">Versaler</label>
+                </div>
+                <div class="form-check form-switch">
+                    <input class="form-check-input" type="checkbox" id="ul" name="riktning" value="ul">
+                    <label class="form-check-label" for="ul">Gemener</label>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
