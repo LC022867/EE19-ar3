c5f7bf979cf1e3fdce6652589209f85de03906d9
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2021-10-12 13:21:32 +0200

    min egen stil av PHP Tutorial (& MySQL)

--- pizza/add.php
+++ pizza/add.php
@@ -8,8 +8,8 @@
 
 if (isset($_GET['submit'])) {
     echo $_POST['email'];
-    echo $_POST['title'];
-    echo $_POST['ingredients'];
+    echo $_POST['username'];
+    echo $_POST['password'];
 }
 
 ?>
@@ -21,14 +21,14 @@ if (isset($_GET['submit'])) {
 <?php include('header.php'); ?>
 
 <section class="container grey-text">
-    <h4 class="center">Add a Pizza</h4>
+    <h4 class="center">Log in</h4>
     <form action="add.php" class="white" method="POST">
         <label for="">Your Email</label>
         <input type="text" name="email">
-        <label for="">Pizza Title</label>
-        <input type="text" name="title">
-        <label for="">Ingredients (comma separated):</label>
-        <input type="text" name="ingredients">
+        <label for="">Username</label>
+        <input type="text" name="username">
+        <label for="">Password</label>
+        <input type="password" name="password">
         <div class="center">
             <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
         </div>
--- pizza/header.php
+++ pizza/header.php
@@ -4,10 +4,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <style type="text/css">
         .brand {
-            background: #cbb09c !important;
+            background: #cc9200 !important;
         }
         .brand-text {
-            color: #cbb09c !important;
+            color: #cc9200 !important;
         }
         form {
             max-width: 460px;
@@ -16,12 +16,12 @@
         }
     </style>
 </head>
-<body class="grey lighten-4">
+<body class="black lighten-5">
     <nav class="white z-depth-0">
         <div class="container">
-            <a href="#" class="brand-logo brand-text">Ninja Pizza</a>
+            <a href="#" class="brand-logo brand-text">MilkFlix</a>
             <ul id="nav-mobile" class="right hide-on-small-and-down">
-                <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+                <li><a href="#" class="btn brand z-depth-0">Like milk?</a></li>
             </ul>
         </div>
     </nav>
