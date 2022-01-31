73599391668db42d65a1931c092b8bb3c67b315f
Luciano Cucarano <luciano.cucarano@elev.ga.ntig.se>
2022-01-31 10:52:35 +0100

    tränat på sql i PHP

---
+++ flash-cards/view/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <?php if ($number) { ?>
+        <a href="index.php">Click Here To Pick Another Number!</a>
+    <?php } else {  ?>
+        🚀🚀🚀🚀🚀🚀
+    <?php } ?>
+
+</footer>
